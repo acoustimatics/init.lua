248e25265e7ec6d72640fb0b5ee10bce254f9690
Christopher Cowan <chrisjoelcowan@gmail.com>
2023-10-08 10:05:57 -0400

    Added lazy.nvim. Added color scheme.

--- init.lua
+++ init.lua
@@ -6,3 +6,22 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.tabstop = 4
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    'Mofiqul/dracula.nvim'
+});
+
+vim.cmd[[colorscheme dracula]]

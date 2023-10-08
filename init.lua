20ecadd14318e4fbf92fa95f32302d477af803d8
Christopher Cowan <chrisjoelcowan@gmail.com>
2023-10-08 11:14:45 -0400

    Added telescope and treesitter.

--- init.lua
+++ init.lua
@@ -21,7 +21,60 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    'Mofiqul/dracula.nvim'
+    "Mofiqul/dracula.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.3",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    "nvim-treesitter/nvim-treesitter"
 });
 
 vim.cmd[[colorscheme dracula]]
+
+do
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+end
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = {
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+
+      "css",
+      "html",
+      "javascript",
+      "rust",
+      "typescript"
+  },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { },
+
+  highlight = {
+    enable = true,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+

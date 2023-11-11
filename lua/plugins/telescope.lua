4d022ab6f611487b0914df11c38a9969f9a81f17
Christopher Cowan <chrisjoelcowan@gmail.com>
2023-11-11 15:37:54 -0500

    Telescope tweaks.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,10 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.3",
+  tag = "0.1.4",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function ()
-    vim.keymap.set("n", "<leader>P", "<cmd>Telescope<cr>")
-
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Files (Telescope)" })
+    vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Buffers (Telescope)" })
   end,
 }

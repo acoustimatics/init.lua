3b7fbd8c842c1cfacc274322ee27f431f4023cfb
Christopher Cowan <chrisjoelcowan@gmail.com>
2023-11-13 21:31:09 -0500

    Tweak which-key help text.

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,6 +3,10 @@ return {
   init = function ()
     vim.opt.timeout = true
     vim.opt.timeoutlen = 1000
+
+    require("which-key").register({
+      ["<leader>c"] = { name = "Code", _ = "which_key_ignore" },
+    })
   end,
   opts = { },
 }

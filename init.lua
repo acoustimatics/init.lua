7e7cdbd7b700a6f7bd14302ec2d6c304b01ce2a7
Christopher Cowan <chrisjoelcowan@gmail.com>
2023-11-13 21:29:36 -0500

    Restoring nvim-tree plugin.

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- The nvim-tree plugin suggests to disable netrw early.
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require("core.options")
 require("core.keymaps")
 require("plugins-init")

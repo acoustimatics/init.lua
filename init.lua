2a3fd34ad2bf8973ad25efd853e51593950dc4fd
Christopher Cowan <chrisjoelcowan@gmail.com>
2025-02-08 12:22:10 -0500

    Deletes nvim-tree plugin.

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
--- The nvim-tree plugin suggests to disable netrw early.
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 require("core.options")
 require("core.keymaps")
 require("plugins-init")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.4",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function ()
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Files (Telescope)" })

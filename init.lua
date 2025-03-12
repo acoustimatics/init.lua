5e97fc7e090753f97eeb82f4e2e19e4459584708
Christopher Cowan <chrisjoelcowan@gmail.com>
2025-03-12 08:47:07 -0400

    Adds Dracula theme and makes it default.

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 require("core.options")
 require("core.keymaps")
 require("plugins-init")
+
+vim.cmd.colorscheme "dracula"
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = { style = "moon" },
-  config = function ()
-    vim.cmd.colorscheme "tokyonight"
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = { style = "moon" },
+  },
+  {
+    "Mofiqul/dracula.nvim"
+  }
 }
